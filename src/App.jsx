61fb2da25a61f03d59f8daacf6dec8ad204a17b9
okstas86 <stanislavocunev1@gmail.com>
2024-02-08 18:53:32 +0200

    Created Header and UserInput components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import UserInput from "./components/UserInput.jsx"
+
+function App() {
+	return (
+		<>
+			<Header />
+			<UserInput />
+		</>
+	)
+}
+
+export default App
